bc4caa377c0ec793b1d87bf0d7365fc61926df9a
RamonTrrl <ing.mrtorrel@gmail.com>
2026-03-10 21:40:49 +0000

    Preguntas 1, 2 y 3 completadas

--- demographic_data_analyzer.py
+++ demographic_data_analyzer.py
@@ -1,17 +1,30 @@
 import pandas as pd
 
+
 def calculate_demographic_data(print_data=True):
-    df = pd.read_csv("adult.data.csv")
-
-# 1.Datos de raza (contar)
-    race_count = df["race"].value_counts()
+    # Leer datos
+    df = pd.read_csv('adult.data.csv')
+    
+    # 1. Conteo por raza
+    race_count = df['race'].value_counts()
+    
+    # 2. Edad media de los hombres
+    average_age_men = round(df[df['sex'] == 'Male']['age'].mean(), 1)
+    # 3. porcentaje con licenciatura
+    total_people = len(df)
+    percentage_bachelors = round((df['education'] == 'Bachelors').sum() / len(df) * 100, 1)
+     
+    
     if print_data:
-        print("numero de personas por raza: ")
+        print('Número de personas por raza:')
         print(race_count)
-
-    return {"race_count": race_count}
-
+        print('Edad media de los hombres:', average_age_men)   
+        print('porcentaje con licenciatura:', percentage_bachelors, '%' )    
+    return {
+        'race_count': race_count,
+        'average_age_men': average_age_men,
+        'percentage_bachelors' : percentage_bachelors  
+    }
+    
 if __name__ == "__main__":
     calculate_demographic_data()
-
-
